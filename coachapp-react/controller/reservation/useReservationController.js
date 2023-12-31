8f8b267298501d2330b31437cdd49e0c1b2b6c1a
Mevin Redureau <mel.redureau@gmail.com>
2023-12-31 14:24:35 +0100

    :construction: WIP on Reservation

--- coachapp-react/controller/reservation/useReservationController.js
+++ coachapp-react/controller/reservation/useReservationController.js
@@ -1,11 +1,56 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 
 export default function useReservationController() {
     const [service, setService] = useState()
     const [dateHoraire, setDateHoraire] = useState(new Date())
+    const [equipiers, setEquipiers] = useState(null)
+
+    async function getEquipier(id_hebergement)  {
+        /*TODO: Appel du view model pour récupérer les équipier en fonction de l'hébergement qui sera fournis
+        *   La fonction retournera un tableau ou un JSON détaillant les différentes horaires et jours de travail
+            en fonction des des équipiers.*/
+
+        return await setTimeout(function(){
+            return [
+                {
+                    "id": 1,
+                    "nom": "Jean",
+                    "jour_travail": ["Lundi", "Mardi", "Mercredi", "Dimanche"]
+                },
+                {
+                    "id": 2,
+                    "nom": "Eude",
+                    "jour_travail": ["Lundi", "Mardi", "Jeudi", "Vendredi"]
+                },
+                {
+                    "id": 3,
+                    "nom": "Myreille",
+                    "jour_travail": ["Lundi", "Mardi", "Mercredi", "Vendredi"]
+                },
+                {
+                    "id": 4,
+                    "nom": "Chloé",
+                    "jour_travail": ["Lundi", "Mardi", "Jeudi", "Samedi"]
+                }
+            ]
+
+        }, 3000)
+    }
+    console.log(equipiers)
+
+    useEffect(() => {
+    setEquipiers(getEquipier(0))
+    }, []);
+
+    console.log(equipiers)
+
 
     return {
-
+        service,
+        setService,
+        dateHoraire,
+        setDateHoraire,
+        equipiers
     }
 }
--- coachapp-react/pages/reservation/index.js
+++ coachapp-react/pages/reservation/index.js
@@ -2,14 +2,23 @@ import Head from "next/head";
 import styles from "@/styles/Home.module.css";
 import { Inter } from 'next/font/google';
 import Navbar from "@/components/UIComponents/navbar";
+import useReservationController from "@/controller/reservation/useReservationController";
+import {log} from "next/dist/server/typescript/utils";
 
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Reservation() {
-    // const {
-    //
-    // } = useReservationController()
+export default function Reservation({id_Hebergement = 0}) {
+    const {
+        service,
+        setService,
+        dateHoraire,
+        setDateHoraire,
+        equipiers,
+    } = useReservationController()
+
+    console.log(equipiers)
+
     return (
         <>
             <Head>
@@ -22,7 +31,7 @@ export default function Reservation() {
                 <Navbar />
                 <div className={`${styles.center} ${styles.marginAuto}`}>
                 </div>
-
+                {equipiers !== null ? <div>HELLO</div> : <div>Waiting...</div>}
             </main>
         </>
     )
