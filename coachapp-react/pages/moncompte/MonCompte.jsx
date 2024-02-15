32baa77639fa639b7c7992fc6b58c948b8789244
robin Sobasto <robin.sobasto94@gmail.com>
2024-02-15 10:08:48 +0100

    change propTypes

--- coachapp-react/pages/moncompte/MonCompte.jsx
+++ coachapp-react/pages/moncompte/MonCompte.jsx
@@ -48,5 +48,5 @@ MonCompte.propTypes = {
     }),
     resetPassword: PropTypes.func.isRequired,
     userForm: PropTypes.any.isRequired, // Adjust the type according to the actual data type
-    seeUserForm: PropTypes.func.isRequired,
+    seeUserForm: PropTypes.bool.isRequired,
   };
